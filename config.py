f4b9a91c572c62bdd841197a3386818b4aac2edc
huryaq <hurryapurrya@gmail.com>
2025-11-07 22:13:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,61 @@
+# +++ Made By Gojo [telegram username: @DoraShin_hlo] +++
+
+import asyncio
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+#Bot token @Botfather, --⚠️ REQUIRED--
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org, --⚠️ REQUIRED--
+APP_ID = int(os.environ.get("APP_ID", "25920083"))
+
+#Your API Hash from my.telegram.org, --⚠️ REQUIRED--
+API_HASH = os.environ.get("API_HASH", "b36177dd5dde3fd0cdbaf1b796d525b8")
+
+#Your db channel Id --⚠️ REQUIRED--
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003273518766"))
+
+#OWNER ID --⚠️ REQUIRED--
+OWNER_ID = int(os.environ.get("OWNER_ID", "5636597174"))
+
+#SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database --⚠️ REQUIRED--
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hurryapurrya:KcRXeLz4N21gcN7F@cluster0.bwp3hkb.mongodb.net/?appName=MuzanXebot")
+DB_NAME = os.environ.get("DATABASE_NAME", "MuzanXe_bot")
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
+PICS = (os.environ.get("PICS", "https://graph.org/file/d7dc886b51feb2ecd169f-313a08a0df20cb3961.png")).split() #Required
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
